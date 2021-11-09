ddf90d2abb7e0ee65af21cecdc14f173ebefa354
ri7az <abdulriiyaz@gmail.com>
2021-11-09 17:59:54 +0530

    ✅✨✨Added Scraped Description

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,39 @@
 const express = require("express");
 const router = express.Router();
 
+//import puppeteer
+const puppeteer = require("puppeteer");
+let character = "";
+
 router.get("/", handleAPIRequest);
 
-function handleAPIRequest(req, res, next) {
-    res.json({ character: req.body.character });
+async function handleAPIRequest(req, res, next) {
+    character = req.body.character;
+    const data = await fetchData(character);
+    res.json({ character: req.body.character, desc: data[0] });
 }
+
+const fetchData = async (c) => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ["--no-sandbox"],
+    });
+    const page = await browser.newPage();
+    await page.goto(`https://berserk.fandom.com/wiki/${c}`);
+
+    const data = await page.evaluate(() => {
+        let contents = [];
+        let content = document.querySelectorAll("p");
+        for (let i = 0; i < content.length; i++) {
+            contents.push(content[i].innerText);
+        }
+        return contents;
+    });
+    //console.log(`https://berserk.fandom.com/wiki/${c}`);
+    //filter the array where the first element is the 'c'
+    //await page.screenshot({ path: "example.png" });
+    await browser.close();
+    return data.filter((e) => e.includes("is a"));
+};
 
 module.exports = router;
